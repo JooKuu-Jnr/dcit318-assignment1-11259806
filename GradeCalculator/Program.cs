f7d13637b3c79aed1e7203529fb8bc2f3bdebaca
Kuuku Ampah <ampahkuuku@gmail.com>
2025-08-05 12:27:48 +0000

    GradeCalculator App

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to JooKuu's Assignment.");
+        Console.WriteLine("Enter a numerical grade between 0 and 100:");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int grade))
+        {
+            if (grade >= 90)
+            {
+                Console.WriteLine("Grade: A");
+            }
+            else if (grade >= 80)
+            {
+                Console.WriteLine("Grade: B");
+            }
+            else if (grade >= 70)
+            {
+                Console.WriteLine("Grade: C");
+            }
+            else if (grade >= 60)
+            {
+                Console.WriteLine("Grade: D");
+            }
+            else
+            {
+                Console.WriteLine("Grade: F");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter a valid number between 0 and 100.");
+        }
+    }
+}
