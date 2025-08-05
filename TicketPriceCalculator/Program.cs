6a8285f423a288c1857c4caafcc19f9c0e2a072c
Kuuku Ampah <ampahkuuku@gmail.com>
2025-08-05 12:36:50 +0000

    Added TicketPriceCalculator code and .gitignore

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class TicketPriceCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to JooKuu's Assignment.");
+        Console.WriteLine("Enter your age:");
+
+        string input = Console.ReadLine();
+        
+        if (int.TryParse(input, out int age))
+        {
+            if (age >= 65 || age <= 12)
+            {
+                Console.WriteLine("Ticket Price: GHC 7");
+            }
+            else
+            {
+                Console.WriteLine("Ticket Price: GHC 10");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter a valid age.");
+        }
+    }
+}
